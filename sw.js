d39cd2fcb63d4b48cc3a8178e8f0c98a610f2afb
dadsondankstry-crypto <dadsondankstry@gmail.com>
2026-01-29 02:32:20 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+{
+  "name": "Editor Pro Mobile",
+  "short_name": "EditorPro",
+  "start_url": "index.html",
+  "display": "fullscreen",
+  "background_color": "#f0f2f5",
+  "theme_color": "#34495e",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2092/2092190.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
